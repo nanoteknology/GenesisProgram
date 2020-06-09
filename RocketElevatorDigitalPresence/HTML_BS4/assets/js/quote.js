d498f2240102a439541b05a3be08399cc2d3ee19
nanoteknology <delita418@gmail.com>
2020-06-09 11:31:53 -0400

    submit.html updated

--- RocketElevatorDigitalPresence/HTML_BS4/assets/js/quote.js
+++ RocketElevatorDigitalPresence/HTML_BS4/assets/js/quote.js
@@ -1,3 +1,7 @@
+// Exemple made by Mathieu Lortie to start our Submission Form
+// drop down menu inside submit.html to choose Residential, Commercial, Corporate or Hybrid Form
+// Shared by Thomas Simoneau (3rd June 2020) @slack @genesis
+
 $(document).ready(function () {
     $('.building_type').hide();
     
